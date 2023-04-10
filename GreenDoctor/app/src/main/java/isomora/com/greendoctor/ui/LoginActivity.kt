247a27b615960063afdeaf08f3f5d7bd54b66da1
Ziad Mahmoud <ziaadaboelela@gmail.com>
2023-04-10 09:23:52 +0200

    login authantication

--- GreenDoctor/app/src/main/java/isomora/com/greendoctor/ui/LoginActivity.kt
+++ GreenDoctor/app/src/main/java/isomora/com/greendoctor/ui/LoginActivity.kt
@@ -3,27 +3,81 @@ package isomora.com.greendoctor.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import com.google.firebase.auth.FirebaseUser
+import isomora.com.greendoctor.MainActivity
 import isomora.com.greendoctor.databinding.ActivityLoginBinding
 import isomora.com.greendoctor.databinding.ActivityMainBinding
 
 class LoginActivity : AppCompatActivity() {
-
+    var mAuth: FirebaseAuth? = null
+    var mUser: FirebaseUser? = null
     lateinit var bindin: ActivityLoginBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindin = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(bindin.root)
 
+        mAuth = FirebaseAuth.getInstance()
+        mUser = mAuth!!.currentUser
+        if (mUser != null) {
+            finish()
+            startActivity(Intent(this, MainActivity::class.java))
+        }
         bindin.textViewSignUp.setOnClickListener {
-            var intent = Intent(this, SignUpActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, SignUpActivity::class.java))
             finish()
         }
 
         bindin.textViewForgetPassword.setOnClickListener {
-            var intent = Intent(this, ForgetPasswordActivity::class.java)
-            startActivity(intent)
+            startActivity(Intent(this, ForgetPasswordActivity::class.java))
             finish()
+        }
+
+        bindin.buttonLogin.setOnClickListener {
+            var email = bindin.editTextEmailLogin.text.toString().trim()
+            var password = bindin.editTextPasswordLogin.text.toString().trim()
+
+            if (email.isEmpty()) {
+                bindin.textInputEmailLogin.error = "should enter your email"
+            } else if (password.isEmpty()) {
+                bindin.textInputPasswordLogin.error = "should enter you password"
+            } else {
+                bindin.progressBar.visibility = View.VISIBLE
+                //login the user
+                login()
+            }
+        }
+
+    }
+
+    private fun login() {
+        var email = bindin.editTextEmailLogin.text.toString().trim()
+        var password = bindin.editTextPasswordLogin.text.toString().trim()
+
+        mAuth?.signInWithEmailAndPassword(email, password)?.addOnCompleteListener() {
+            if (it.isSuccessful) {
+                verifyEmail()
+            } else {
+                bindin.progressBar.visibility = View.INVISIBLE
+                Toast.makeText(this, "Account doesn't exists", Toast.LENGTH_SHORT).show()
+            }
+
+        }
+    }
+
+    private fun verifyEmail() {
+        var user = mAuth!!.currentUser
+        if (user!!.isEmailVerified) {
+            Toast.makeText(this, "Email Verified", Toast.LENGTH_SHORT).show()
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        } else {
+            bindin.progressBar.visibility = View.INVISIBLE
+            Toast.makeText(this, "Verify your email first", Toast.LENGTH_SHORT).show()
+            mAuth!!.signOut()
         }
     }
 }
--- GreenDoctor/app/src/main/java/isomora/com/greendoctor/ui/SignUpActivity.kt
+++ GreenDoctor/app/src/main/java/isomora/com/greendoctor/ui/SignUpActivity.kt
@@ -3,20 +3,80 @@ package isomora.com.greendoctor.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Toast
+import com.google.firebase.auth.FirebaseAuth
+import isomora.com.greendoctor.MainActivity
 import isomora.com.greendoctor.databinding.ActivityLoginBinding
 import isomora.com.greendoctor.databinding.ActivitySignUpBinding
 
 class SignUpActivity : AppCompatActivity() {
+    var mAuth: FirebaseAuth? = null
     lateinit var bindin: ActivitySignUpBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         bindin = ActivitySignUpBinding.inflate(layoutInflater)
         setContentView(bindin.root)
 
+        mAuth = FirebaseAuth.getInstance()
+
         bindin.textViewLogin.setOnClickListener {
             var intent = Intent(this, LoginActivity::class.java)
             startActivity(intent)
             finish()
         }
+
+        bindin.buttonSignUp.setOnClickListener {
+            Register()
+        }
+    }
+
+    private fun Register() {
+        var email = bindin.editTextEmailSignUp.text.toString().trim()
+        var password = bindin.editTextPasswordSignUp.text.toString().trim()
+        var confirm = bindin.editTextConfirmPasswordSignUp.text.toString().trim()
+
+        if (email.isEmpty()) {
+            bindin.textInputEmailSignUp.error = "enter your mail"
+        } else if (password.isEmpty() || password.length < 7) {
+            bindin.textInputPasswordSignUp.error = "the password should be greater than 7"
+        } else if (confirm != password) {
+            bindin.textInputConfirmPasswordSignUp.error = "the password not same"
+        } else {
+            //Registr the user to firebase
+
+            mAuth!!.createUserWithEmailAndPassword(email, password).addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(this, "Register success", Toast.LENGTH_SHORT).show()
+                    sendEmailVerification()
+
+                } else {
+                    Toast.makeText(this, "Register fail", Toast.LENGTH_SHORT).show()
+
+                }
+
+            }
+        }
+    }
+
+    private fun sendEmailVerification() {
+        var user = mAuth!!.currentUser
+
+        if (user != null) {
+            user.sendEmailVerification().addOnCompleteListener {
+                if (it.isSuccessful) {
+                    Toast.makeText(
+                        this,
+                        "Verification Email is Sent,Verify and log In Again",
+                        Toast.LENGTH_SHORT
+                    ).show()
+                    mAuth!!.signOut()
+                    finish()
+                    startActivity(Intent(this, LoginActivity::class.java))
+                } else {
+                    Toast.makeText(this, it.exception.toString(), Toast.LENGTH_SHORT).show()
+                }
+            }
+        }
+
     }
 }
